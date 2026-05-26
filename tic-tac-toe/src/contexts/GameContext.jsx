fba72ee2f654e090758005471e52751500bc31d7
Alyessa Moodley <aly711@live.com>
2026-05-26 09:45:01 +0200

    Added comments

--- tic-tac-toe/src/contexts/GameContext.jsx
+++ tic-tac-toe/src/contexts/GameContext.jsx
@@ -9,12 +9,16 @@ export const GameContext = createContext({});
 
 export const GameContextProvider = ({ children }) => {
 
+    // This allows components to access and update state without prop drilling.
+    // Game - current global state
+    // dispatch - function used to trigger actions
     const [game, dispatch] = useReducer(
         gameReducer,
         initialState
     );
 
     return (
+        // This makes the game state available globally to all components
         <GameContext.Provider value={{ game, dispatch }}>
             {children}
         </GameContext.Provider>
